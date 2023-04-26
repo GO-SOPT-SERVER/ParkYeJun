90de22ecfb1f80b44685daf293032bfc812f6f0c
jun02160 <jun020216@naver.com>
2023-04-27 00:22:35 +0900

    [ADD] 3주차 세미나 실습 코드

--- WEEK3/ThirdSeminar/src/main/java/sort/org/ThirdSeminar/common/advice/ControllerExceptionAdvice.java
+++ WEEK3/ThirdSeminar/src/main/java/sort/org/ThirdSeminar/common/advice/ControllerExceptionAdvice.java
@@ -11,6 +11,8 @@ import sort.org.ThirdSeminar.exception.ErrorStatus;
 /*
 Custom한 에러 Response를 적용하여 반환하기 위해 @RestControllerAdvice를 사용하여
 모든 Controller에 대해 전역적으로 발생할 수 있는 예외를 잡고 처리한다.
+
+-> @Controller, @RestController 에서 발생하는 예외에만 적용이 가능하다.
 */
 @RestControllerAdvice
 public class ControllerExceptionAdvice {
