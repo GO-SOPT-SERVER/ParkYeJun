c8443d324ac75abcb2ef39cf489d318b9b46cbfc
jun02160 <jun020216@naver.com>
2023-05-02 20:11:25 +0900

    [fix] 연관관계 메서드 수정 및 주석 추가

--- WEEK3/ThirdSeminar/src/main/java/sort/org/ThirdSeminar/domain/Post.java
+++ WEEK3/ThirdSeminar/src/main/java/sort/org/ThirdSeminar/domain/Post.java
@@ -39,8 +39,15 @@ public class Post extends BaseTimeEntity {
         this.category = category;
     }
 
+    // 연관관계의 주인만이 외래키를 관리할 수 있고, 실제 DB에 반영이 가능하게 한다. 따라서 양방향 연관관계를 설정하는 부분은 관계의 주인인 Post가 가지고 있어야 한다.
     public void setWriter(User user) {
-        this.writer = user;
-        writer.getPostList().add(this);
+
+        // 기존의 관계를 제거 *사실 작성자가 변경되는 경우는 거의 없으나, JPA 연관관계 매핑 시 변경될 수 있는 경우를 고려하여 기존의 관계를 끊어주는 처리도 필요하다. (양방향 모두 끊어지도록)
+        if (this.writer != null) {
+            this.writer.getPostList().remove(this);
+        }
+
+        this.writer = user;  // Post -> Member 방향으로 연관관계 설정 (연관관계의 주인
+        writer.getPostList().add(this);  // Member -> Post 방향 : DB에서는 무시하는 로직이지만, 순수한 객체의 관점에서 동작하려면 꼭 양방향으로 연결해주는 것이 필요하다.
     }
 }
