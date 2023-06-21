19ffb05aeda075903e5f5a2d6f62fe3dbc11cc76
jun02160 <jun020216@naver.com>
2023-06-21 15:28:54 +0900

    [ADD] RedisConfig

--- WEEK4/FourthSeminar/src/main/java/sopt/org/FourthSeminar/FourthSeminarApplication.java
+++ WEEK4/FourthSeminar/src/main/java/sopt/org/FourthSeminar/FourthSeminarApplication.java
@@ -3,7 +3,9 @@ package sopt.org.FourthSeminar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession  // Redis로 session을 관리하도록 설정
 @EnableJpaAuditing  // 이 어노테이션이 있어야 변경 사항을 감지할 수 있다.
 @SpringBootApplication
 public class FourthSeminarApplication {
